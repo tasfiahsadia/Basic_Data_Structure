376ed7a89cd32fc2c6bcc257bc062b323ff9ab4b
tasfiahsadia <tasfiahsadia@gmail.com>
2026-07-20 19:47:14 +0600

    Implement stack using STL list

---
+++ Practice_13/Implement_stack_using_STL_list.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Stack{
+    public:
+        list<int> l;
+
+        void push(int val){
+            l.push_back(val);
+        }
+        void pop(){
+            l.pop_back();
+        }
+        int top(){
+            return l.back();
+        }
+        int size(){
+            return l.size();
+        }
+        bool empty(){
+            return l .empty();
+        }
+};
+
+int main(){
+    Stack st;
+
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while(!st.empty()){
+        cout << st.top() << " ";
+        st.pop();
+    }
+}
