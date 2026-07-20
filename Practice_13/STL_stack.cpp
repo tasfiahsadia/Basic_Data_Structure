e57c4b9f3029e28cb4a18c1c1ba7f04ac794257e
tasfiahsadia <tasfiahsadia@gmail.com>
2026-07-20 20:19:32 +0600

    STL Stack

---
+++ Practice_13/STL_stack.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int> st;
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    // if(!st.empty()) loop chara acess korte
+    //     st.top();
+    // if(!st.empty())
+    //     st.pop();
+    
+    while(!st.empty()){
+        cout << st.top() << " ";
+        st.pop();
+    }
+}
