3b2fde3c6aaa0a56ee2f3f70a0efb7729c6f7a90
tasfiahsadia <tasfiahsadia@gmail.com>
2026-07-21 20:19:05 +0600

    Implement Queue using Doubly Linked List

---
+++ Practice_14/Implement_queue_using_doubly.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+        int value;
+        node* next;
+        node* prev;
+
+        node(int val){
+            this->value = val;
+            this->next = NULL;
+            this->prev = NULL;
+        }
+};
+
+class myQueue{
+    public:
+    node* head = NULL;
+    node* tail = NULL;
+    int size = 0;
+
+    void push(int val){
+        size++;
+        node* newNode = new node(val);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+
+    void pop(){
+        size--;
+        node* toDelete = head;
+        head = head->next;
+        delete toDelete;
+
+        if(head == NULL){
+            tail = NULL;
+            return;
+        }
+        head->prev = NULL;
+    }
+
+    int front(){
+        return head->value;
+    }
+
+    int back(){
+        return tail->value;
+    }
+
+    int getSize(){
+        return size;
+    }
+
+    bool empty(){
+        return head == NULL;
+    }
+};
+
+int main(){
+    myQueue q;
+    int n;
+    cin >> n;    
+
+    for(int i = 0; i < n; i++){
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+
+    while(!q.empty()){
+        cout << q.front() << endl;
+        q.pop();
+    }
+}
